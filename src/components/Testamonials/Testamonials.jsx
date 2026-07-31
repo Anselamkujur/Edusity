2284492ace78d0907f61fe52c20deeaafe60e8bf
Anselamkujur <143241307+Anselamkujur@users.noreply.github.com>
2026-07-31 22:30:37 +0530

    Testamonials component is added

--- src/components/Testamonials/Testamonials.jsx
+++ src/components/Testamonials/Testamonials.jsx
@@ -1,10 +1,87 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './Testamonials.css'
+import next_icon from '../../assets/next-icon.png'
+import back_icon from '../../assets/back-icon.png'
+import user_1 from '../../assets/user-1.png'
+import user_2 from '../../assets/user-2.png'
+import user_3 from '../../assets/user-3.png'
+import user_4 from '../../assets/user-4.png'
+
 
 const Testamonials = () => {
+  const slider = useRef();
+  let tx = 0;
+  const slideForward = () => {
+    if (tx > -50) {
+      tx -= 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`
+
+  }
+
+  const slideBackward = () => {
+     if (tx < 0) {
+      tx += 25;
+    }
+    slider.current.style.transform = `translateX(${tx}%)`
+
+  }
   return (
     <div className='testamonials'>
-      
+      <img src={next_icon} className='next-btn' onClick={slideForward} />
+      <img src={back_icon} className='back-btn' onClick={slideBackward} />
+      <div className="slider">
+        <ul ref={slider}>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_1} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus tempora amet, accusamus nesciunt quasi iusto possimus error alias asperiores.</p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_2} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus tempora amet, accusamus nesciunt quasi iusto possimus error alias asperiores.</p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_3} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus tempora amet, accusamus nesciunt quasi iusto possimus error alias asperiores.</p>
+            </div>
+          </li>
+          <li>
+            <div className="slide">
+              <div className="user-info">
+                <img src={user_4} alt="" />
+                <div>
+                  <h3>William Jackson</h3>
+                  <span>Edusity, USA</span>
+                </div>
+              </div>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa doloribus tempora amet, accusamus nesciunt quasi iusto possimus error alias asperiores.</p>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
